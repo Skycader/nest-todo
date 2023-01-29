194bb01f7d8525564be531232b26639fb6b96d46
skycader <skycader@github.com>
2023-01-29 16:57:30 +0300

    getById

--- src/modules/todos/controllers/todos/todos.controller.ts
+++ src/modules/todos/controllers/todos/todos.controller.ts
@@ -1,15 +1,26 @@
+import { AddTodoDto } from './../../dtos/add-todo.dto';
 import { TodosService } from './../../../todos/services/todos/todos.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Todo } from '../../models/todo.model';
 
 @Controller('todos')
 export class TodosController {
-  constructor(private todosService: TodosService) {
-
-  }
+  constructor(private todosService: TodosService) {}
 
   @Get()
-  getAllTodos():Todo[] {
-    return this.todosService.getAllTodos()
+  getAllTodos(): Todo[] {
+    return this.todosService.getAllTodos();
+  }
+
+  @Get('/:id')
+  getTodoById(@Param('id') id: string ) {
+    return this.todosService.
+  }
+
+  @Post()
+  addTodo(
+    @Body() addTodoDto: AddTodoDto
+  ): Todo {
+    return this.todosService.addTodo(addTodoDto);
   }
 }
--- src/modules/todos/services/todos/todos.service.ts
+++ src/modules/todos/services/todos/todos.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { Todo } from '../../models/todo.model';
-
+import { Todo, TodoStatus } from '../../models/todo.model';
+import { v4 as uuid } from 'uuid';
+import { AddTodoDto } from '../../dtos/add-todo.dto';
 @Injectable()
 export class TodosService {
-  private todos: Todo[] = []
+  private todos: Todo[] = [];
 
   getAllTodos(): Todo[] {
     return this.todos;
+  }
+
+  getTodo(id: string) {
+    return this.todos.find(todo => todo.id == id);
+  }
+
+  addTodo(addTodoDto: AddTodoDto): Todo {
+    const { title, description } = addTodoDto
+    const todo: Todo = {
+      id: uuid(),
+      title,
+      description,
+      status: TodoStatus.OPEN,
+    };
+
+    this.todos.push(todo);
+    return todo;
   }
 }
